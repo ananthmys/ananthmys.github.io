8ed90bb930a760521637442412318db29d82f936
ananthmysore <ananthj87@gmail.com>
2016-01-20 01:18:13 +0530

    tour details page modified

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,18 +65,18 @@ $(document).ready(function($) {
         Smooth Scroll on anchors
     =============================================== */  
 
-    $('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html,body').animate({
-                  scrollTop: target.offset().top -66
-            }, 1000);
-            return false;
-          }
-        }
-    });
+    // $('a[href*=#]:not([href=#])').click(function() {
+    //     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    //       var target = $(this.hash);
+    //       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    //       if (target.length) {
+    //         $('html,body').animate({
+    //               scrollTop: target.offset().top -66
+    //         }, 1000);
+    //         return false;
+    //       }
+    //     }
+    // });
 
     /* ==============================================
      Bootstrap Tooltip
